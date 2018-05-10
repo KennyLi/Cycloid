c93811ee45972f2a9d2eafb877e91c08b544f6bb
Kenny <lli15@stuy.edu>
2018-05-10 08:34:39 -0400

    implement

---
+++ v2/PriorityQueue.java
@@ -0,0 +1,14 @@
+public class PriorityQueue {
+    //returns true if queue is empty
+    public boolean isEmpty();
+
+    //returns minimum value if not empty
+    public String peekMin();
+
+    //removes minimum value from queue
+    //returns removed value
+    public String removeMin();
+
+    //adds value to queue based on its priority
+    public void add(String input);
+}
